b73f0bc0f88217911e207911113b06948534f01b
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2020-01-25 22:21:52 +0300

    add google client secret

--- services/app/config/config.exs
+++ services/app/config/config.exs
@@ -3,7 +3,8 @@ use Mix.Config
 
 config :what_is_better_to_pay,
   bot_name: System.get_env("TELEGRAM_BOT_NAME"),
-  import_document_example_url: System.get_env("IMPORT_DOCUMENT_EXAMPLE_URL")
+  import_document_example_url: System.get_env("IMPORT_DOCUMENT_EXAMPLE_URL"),
+  google_client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
 
 config :nadia,
   token: System.get_env("TELEGRAM_BOT_TOKEN")
