54744ec230ac81c61cbff62d6deaa6e18dfdd07e
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2020-01-26 14:52:49 +0300

    add skeleton for load bonus program process

--- services/app/lib/what_is_better_to_pay/commands.ex
+++ services/app/lib/what_is_better_to_pay/commands.ex
@@ -1,15 +1,21 @@
 defmodule WhatIsBetterToPay.Commands do
   use WhatIsBetterToPay.Router
   use WhatIsBetterToPay.Commander
-  alias WhatIsBetterToPay.Commands.{Greetings, LocationQuery}
+  alias WhatIsBetterToPay.Commands.{Greetings, LocationQuery, LoadBonusPrograms}
+  alias WhatIsBetterToPay.Services.GoogleDocs
 
   command "start", Greetings, :hello
 
   location_query LocationQuery, :execute
 
   message do
-    Logger.log :warn, "Did not match the message"
-    Logger.log :info, "#{Poison.encode! update.message}"
-    send_message "Sorry, I couldn't understand you"
+    cond do
+      GoogleDocs.valid_link?(update.message.text) ->
+        LoadBonusPrograms.execute(update)
+      true ->
+        Logger.log :warn, "Did not match the message"
+        Logger.log :info, "#{Poison.encode! update.message}"
+        send_message "Sorry, I couldn't understand you"
+    end
   end
 end
---
+++ services/app/lib/what_is_better_to_pay/commands/load_bonus_programms.ex
@@ -0,0 +1,11 @@
+defmodule WhatIsBetterToPay.Commands.LoadBonusPrograms do
+  use WhatIsBetterToPay.Commander
+
+  def execute(_update) do
+    # TODO:
+    # 1. create user(username, first_name, last_name)
+    # 2. download document from google docs
+    # 3. load bonus programms from google docs
+    # 4. send sucess message
+  end
+end
