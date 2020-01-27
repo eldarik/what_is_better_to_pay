6479f33be1e3e4f5c7435b64f41f9c637e356a81
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2020-01-27 13:57:41 +0300

    update test google docs result

--- services/app/lib/what_is_better_to_pay/services/google_docs/test.ex
+++ services/app/lib/what_is_better_to_pay/services/google_docs/test.ex
@@ -2,17 +2,17 @@ defmodule WhatIsBetterToPay.Services.GoogleDocs.Test do
   def fetch_document(_link) do
     ~S"""
     карта,процент,категория,место
-    TINKOFF BLACK 1234,5%,жд,-
-    TINKOFF BLACK 1234,5%,кино,-
-    TINKOFF BLACK 1234,5%,транспорт,-
-    TINKOFF BLACK 1234,5%,аптеки,-
-    TINKOFF BLACK 1234,5%,красота,-
-    TINKOFF BLACK 1234,5%,спорттовары,-
-    ROCKET BANK 1234,8%,-,букет столицы
-    ROCKET BANK 1234,5%,-,terranova
-    ROCKET BANK 1234,5%,-,beanhearts coffee
-    TINKOFF ALL AIRLINES 1234,2%,*,*
-    TINKOFF BLACK 1234,1%,*,*
+    TINKOFF BLACK 1234,5,жд,-
+    TINKOFF BLACK 1234,5,кино,-
+    TINKOFF BLACK 1234,5,транспорт,-
+    TINKOFF BLACK 1234,5,аптеки,-
+    TINKOFF BLACK 1234,5,красота,-
+    TINKOFF BLACK 1234,5,спорттовары,-
+    ROCKET BANK 1234,10,цветы,букет столицы
+    ROCKET BANK 1234,8,одежда,terranova
+    ROCKET BANK 1234,5,кофейня,beanhearts coffee
+    TINKOFF ALL AIRLINES 1234,1,*,*
+    TINKOFF BLACK 1234,2,*,*
     """
   end
 end
