250b2674e30031b7d7908832854b63bfe522b186
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2020-01-30 00:02:16 +0300

    change logger config for test env

--- services/app/config/test.exs
+++ services/app/config/test.exs
@@ -7,3 +7,5 @@ config :what_is_better_to_pay, WhatIsBetterToPay.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   url: System.get_env("DATABASE_URL"),
   ownership_timeout: 99_999_999
+
+config :logger, level: :error
