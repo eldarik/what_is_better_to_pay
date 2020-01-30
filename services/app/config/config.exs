c70c4e3740224cb8cd8d26d92607ef67a8ab4cab
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2020-01-30 18:41:39 +0300

    override init/2 method in repo, try to fix issue with url

--- services/app/config/config.exs
+++ services/app/config/config.exs
@@ -9,8 +9,7 @@ config :what_is_better_to_pay,
 
 config :what_is_better_to_pay, WhatIsBetterToPay.Repo,
   url: System.get_env("DATABASE_URL"),
-  pool_size: 10,
-  show_sensitive_data_on_connection_error: true
+  pool_size: 10
 
 config :nadia,
   token: System.get_env("TELEGRAM_BOT_TOKEN")
--- services/app/lib/what_is_better_to_pay/repo.ex
+++ services/app/lib/what_is_better_to_pay/repo.ex
@@ -2,4 +2,8 @@ defmodule WhatIsBetterToPay.Repo do
   use Ecto.Repo,
     otp_app: :what_is_better_to_pay,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_, opts) do
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  end
 end
