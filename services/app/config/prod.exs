6579940bda156d60aa43ecc76dc590c09289aa79
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2020-01-30 18:19:00 +0300

    add debug lods in production env

--- services/app/config/prod.exs
+++ services/app/config/prod.exs
@@ -1,4 +1,5 @@
 use Mix.Config
 
 config :what_is_better_to_pay,
-  google_docs_api: WhatIsBetterToPay.Services.GoogleDocs
+  google_docs_api: WhatIsBetterToPay.Services.GoogleDocs,
+  show_sensitive_data_on_connection_error: true
