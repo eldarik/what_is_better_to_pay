058862ef5f6bd838b0a2ee88351cf92bc3eea4c5
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2020-02-01 12:35:17 +0300

    update make compose commands

--- make-compose.mk
+++ make-compose.mk
@@ -6,8 +6,36 @@ compose-build:
 compose:
 	docker-compose up app
 
-compose-bash:
-	docker-compose run --user=$(USER) app bash
+compose-down:
+	docker-compose down -v || true
 
-compose-setup: ansible-development-setup-env \
-	compose-build
+compose-bash:
+	docker-compose run --rm --user=$(USER) app bash
+
+compose-db-init:
+	docker-compose run --rm app mix ecto.create
+	docker-compose run --rm app mix ecto.migrate
+
+compose-install-mix:
+	docker-compose run --rm app mix deps.get
+
+compose-setup: compose-down \
+	ansible-development-setup-env \
+	compose-build \
+	compose-install-mix \
+	compose-db-init
+
+compose-console:
+	docker-compose run --rm app iex -S mix
+
+compose-test:
+	docker-compose run --rm app mix test
+
+compose-restart:
+	docker-compose restart
+
+compose-stop:
+	docker-compose stop
+
+compose-logs:
+	docker-compose logs -f --tail=100
