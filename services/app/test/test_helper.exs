d2dc16bb278bf7305092717ece601a579fa77d5d
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2020-02-01 17:10:48 +0300

    comment out ex machine

--- services/app/test/test_helper.exs
+++ services/app/test/test_helper.exs
@@ -4,4 +4,4 @@ ExUnit.configure(timeout: :infinity, exclude: [pending: true], trace: false)
 
 Ecto.Adapters.SQL.Sandbox.mode(WhatIsBetterToPay.Repo, :manual)
 
-{:ok, _} = Application.ensure_all_started(:ex_machina)
+# {:ok, _} = Application.ensure_all_started(:ex_machina)
