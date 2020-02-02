68db8179ad9f8daac0bbcaa92e4f0c79d432118c
Eldar Iskhakov <iskhakov.eldar95@gmail.com>
2020-02-02 16:08:28 +0300

    fix link to example document in greetings command

--- services/app/lib/what_is_better_to_pay/commands/greetings.ex
+++ services/app/lib/what_is_better_to_pay/commands/greetings.ex
@@ -1,25 +1,23 @@
 defmodule WhatIsBetterToPay.Commands.Greetings do
   use WhatIsBetterToPay.Commander
 
-  @import_document_example_url Application.get_env(
-                                 :what_is_better_to_pay,
-                                 :import_document_example_url
-                               )
-
   def hello(update) do
+    import_document_example_url =
+      Application.get_env(:what_is_better_to_pay, :import_document_example_url)
+
     # TODO: move to localized templates, find something like I18n in ruby
-    send_message(~s"""
+    send_message("""
     Здравствуйте, #{update.message.from.username}!
     Для того, чтобы начать пользоваться ботом ему необходимо сообщить
     данные о ваших категориях кэшбека:
-    1. создайте копию таблицы - #{@import_document_example_url}
+    1. создайте копию таблицы - #{import_document_example_url}
     2. заполните ваши категории
     3. дайте доступ по ссылке к созданной таблице
     4. отправьте в сообщении ссылку на документ
     5. в дальнейшем бот будет использовать эти данные
     """)
 
-    send_message(~s"""
+    send_message("""
     Для того, чтобы узнать, чем лучше расплатиться, вы можете:
     1. отправить вашу геолокацию, бот найдет ближайшее место и посоветует вам чем лучше платить
     2. отправить сообщением категорию(кафе, ресторан, такси)
